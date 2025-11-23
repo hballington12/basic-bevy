4c9ef44def1d1ae64e1279784cd2e3a1e32573ee
hballington12 <hballington12@gmail.com>
2025-11-23 15:52:04 +0100

    add a basic circle render

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::prelude::*;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut app = App::new(); // create new app
+
+    app.add_plugins(DefaultPlugins); // add default plugins
+
+    app.add_systems(Startup, setup);
+
+    app.run(); // run the app
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+
+    let shape = meshes.add(Circle::new(50.0));
+    commands.spawn((
+        Mesh2d(shape),
+        MeshMaterial2d(materials.add(Color::hsl(180.0, 1.0, 0.5))),
+    ));
 }
